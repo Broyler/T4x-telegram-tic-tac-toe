23553a73b6ae8c0c2ed2176a36cdb46d991c0271
Arseny <69675212+Broyler@users.noreply.github.com>
2025-07-08 13:13:37 +0300

    Initial project startup
    UV environment, ping command

---
+++ main.py
@@ -0,0 +1,25 @@
+from os import getenv
+from aiogram import Bot, Dispatcher, types
+from dotenv import load_dotenv
+import asyncio
+
+from aiogram.filters.command import Command
+
+load_dotenv()
+token = getenv('TOKEN')
+assert token is not None, "Token is not set! Make sure .env file exists."
+bot = Bot(token=token)
+dp = Dispatcher()
+
+
+@dp.message(Command('ping'))
+async def ping(message: types.Message):
+    await message.reply('pong')
+
+
+async def main():
+    await dp.start_polling(bot)
+
+
+if __name__ == '__main__':
+    asyncio.run(main())
